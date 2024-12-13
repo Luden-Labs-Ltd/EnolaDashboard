a72cb4d6c2aedfd815548940583bdc2c1a341315
Philip Veremeichik <p.veremeichik@richbrains.net>
2024-12-13 11:27:18 +0300

    add dinamic page

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import Dashboard from "page/dashboard";
 
+export const dynamic = 'force-dynamic';
 export default async function DashboardPage() {
   return <Dashboard/>
 }
--- src/app/(main)/resources/page.tsx
+++ src/app/(main)/resources/page.tsx
@@ -3,6 +3,7 @@ import { convertCategoryData, getCategoriesApi } from "entities/category";
 import { getResources } from "entities/resources";
 import Resources from "page/resources";
 
+export const dynamic = 'force-dynamic';
 export default async function ResourcesPage() {
   const profile = await getCurrentProfileApi();
   const programId = profile?.company.programs[0].id ?? null;
