14a230e0b5427d116899fa5bd8cfe3bddf0c4c3d
Philip Veremeichik <p.veremeichik@richbrains.net>
2024-12-23 11:27:02 +0300

    change rtl layout

--- src/widgets/Header/Breadcrumb/HomeMenu/index.tsx
+++ src/widgets/Header/Breadcrumb/HomeMenu/index.tsx
@@ -1,21 +1,32 @@
 import { BreadcrumbLink } from "@components/shadowCDN/breadcrumb";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@components/shadowCDN/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@components/shadowCDN/dropdown-menu";
 import { ChevronDown } from "lucide-react";
 import React from "react";
-import { NAVIGATION_ITEMS } from "shared/constants/navbar";
-import styles from "../breadcrumb.module.scss"
+import { AVAILABLE_PATHS_ALIAS, NAVIGATION_ITEMS } from "shared/constants/navbar";
+import styles from "../breadcrumb.module.scss";
+import { useTranslations } from "next-intl";
 
 interface HomeMenuProps {
-    firstPathKey: string
-    isFirstPathRoot: boolean;
+  firstPathKey: string;
+  isFirstPathRoot: boolean;
 }
-export const HomeMenu: React.FC<HomeMenuProps> = ({isFirstPathRoot, firstPathKey}) => {
+export const HomeMenu: React.FC<HomeMenuProps> = ({
+  isFirstPathRoot,
+  firstPathKey,
+}) => {
+  const t = useTranslations("Paths");
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger
         className={`flex items-center gap-1 ${styles.RouteName}`}
       >
-        {isFirstPathRoot ? firstPathKey : "Home"}
+        {isFirstPathRoot ? firstPathKey : t("home")}
         <ChevronDown className="h-4 w-4" />
       </DropdownMenuTrigger>
       <DropdownMenuContent align="start">
@@ -32,7 +43,7 @@ export const HomeMenu: React.FC<HomeMenuProps> = ({isFirstPathRoot, firstPathKey
               className={`${activeClass}`}
             >
               <BreadcrumbLink href={item.navigateTo}>
-                {item.translateKey}
+                {t(item.translateKey as AVAILABLE_PATHS_ALIAS)}
               </BreadcrumbLink>
             </DropdownMenuItem>
           );
